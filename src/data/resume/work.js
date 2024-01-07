74f314bee963a2b1655d4b4e1e017296f4aeda96
alexyev <85699087+alexyev@users.noreply.github.com>
2024-01-07 18:08:18 -0500

    update

--- src/data/resume/work.js
+++ src/data/resume/work.js
@@ -13,156 +13,73 @@
  */
 const work = [
   {
-    name: 'Smile Identity',
-    position: 'VP Engineering & Head of AI',
-    url: 'https://smileidentity.com',
-    startDate: '2022-01-01',
-    summary: `Smile builds machine learning APIs to compare user submitted photos to ID documents. Our APIs are
-    used by hundreds of thousands of people every day to access financial services. As VP of Engineering,
-    I provide technical leadership on high-impact projects, influence and coach a distributed team of engineers,
-    and facilitate alignment and clarity across teams on goals, outcomes, and timelines. I was promoted
-    from Director of Engineering to VP of Engineering in April 2022, and then to VP of Engineering and Head
-    of AI in November 2022. I lead a 20+ person engineering org. I directly manage ~8 engineers and spend
-    >50% of my time writing code.`,
-    highlights: [
-      'Redesigned engineering processes for bug tracking, meetings, and standups. Improved culture for code reviews, blameless post-mortems, and retrospectives.',
-      'Re-architected engineering hiring and onboarding processes. Recruited several strong engineers.',
-      'Reorged engineering teams to focus on product delivery. Created a new team to focus on ML infrastructure.',
-      'Lead re-design of internal APIs for inference. Built new computer vision pipelines for industry leading certifications (NIST/iBeta liveness).',
-      'Pitched, designed, developed, deployed, and maintain a fraud detection product based on 1-N facial recognition using embeddings and vector search.',
-    ],
-  },
-  {
-    name: 'Skeptical Investments',
+    name: 'Cultura',
     position: 'Co-founder',
-    url: 'http://skepticalinvestments.biz',
-    startDate: '2017-04-01',
-    summary: `Skeptical Investments is a micro-vc fund focused on Angel, Seed, and Series-A
-    YC companies.`,
+    url: 'https://www.culturafarms.ca/landing.html',
+    startDate: '2023-06-01',
     highlights: [
-      'Created the InstaSafe. Fast, automated investment docs for YC companies.',
-      'Advise several startups in the fund.',
+      'Co-Founded app for local farmers including expenditure, crop phenology, and weather',
+      'Won BETA Camp Hackathon and Buildspace Top 32, raised grant from Emergent Ventures',
     ],
   },
   {
-    name: 'Arthena',
-    position: 'Co-founder & C.T.O.',
-    url: 'https://arthena.com',
-    startDate: '2014-01-01',
-    endDate: '2022-01-01',
-    summary: `Arthena is a Series A Company funded by <a href="https://www.anthemis.com/">Athemis</a>,
-    <a href="https://foundationcapital.com">Foundation Capital</a>, and <a href="https://ycombinator.com/">YC</a>.
-    We develop quantitative strategies to predict the value of fine art and build investment products.
-    I ran a 20-person product and engineering org for 8 years. We were acquired by
-    <a href="https://www.masterworks.com/">Masterworks</a> in 2023.`,
+    name: 'CAE',
+    position: 'Consultant, PM',
+    url: 'https://www.cae.com/',
+    startDate: '2023-02-01',
+    endDate: '2014-03-01',
     highlights: [
-      'Built production, online, end-to-end optimized machine learning pipelines for time-series prediction.',
-      'Designed micro-service architecture around: data collection, data integrity, feature engineering, research, strategy, backtesting, deployment, and reporting.',
-      "Set and communicated team priorities that supported the broader organization's goals. Aligned strategy, processes, and decision-making across teams.",
-      "Set clear expectations with individuals based on their level and role and aligned them to the broader organization's goals. Met regularly with individuals to discuss performance and development, and provided feedback and coaching.",
-      'Developed the long-term technical vision and roadmap within, and often beyond, the scope of my teams. Evolved the roadmap to meet anticipated future requirements and infrastructure needs.',
+      'Tasked with identifying new market verticals for existing hardware and infrastructure to boost revenue',
+      'Month-long intensive research and testing project included meeting users and creating business forecasts',
+      'Created a full pitch deck outlining team\'s solution, presented to CAE board',
+      'As Project Manager, assgined and delegated work, kept team on task and created deadlines for projects',
     ],
   },
   {
-    name: 'Enveritas',
-    position: 'ML Engineering Contractor',
-    url: 'http://enveritas.org',
-    startDate: '2016-09-01',
-    endDate: '2018-01-01',
-    summary: `Enveritas is a non-profit that works with coffee farmers to improve their livelihoods.
-    I joined on day one and helped build a variety of tools to collect and analyze data. I also advised
-    on technical strategy, helped hire engineers, and advised on their YC application.`,
+    name: 'The Catherine Wheel',
+    position: 'Intern',
+    url: 'https://www.thecatherinewheel.co/',
+    startDate: '2022-07-01',
+    endDate: '2022-08-01',
     highlights: [
-      'Trained a model for the Brazilian Coffee Scenes Dataset with better than state of the art accuracy.',
-      'Collected training sets on the ground in Uganda. Built dashboards to visualize work of surveyors using Flask, React, and D3.',
-      'Performed supporting analysis to ensure data integrity using Pandas, t-SNE, SVM\'s, and other techniques.',
+      'Conducted market research on emerging tech verticals and potential investors',
+      'Aided 3 current portfolio companies with market insight and vetted prospective investments',
     ],
   },
   {
-    name: 'Zenysis',
-    position: 'Software Engineering Contractor',
-    url: 'https://zenysis.com',
-    startDate: '2016-02-01',
-    endDate: '2016-03-01',
-    summary: `Zenysis develops data analysis and visualization tools for government agencies and international organizations.
-    Their solutions help improve decision-making by integrating and analyzing large-scale datasets from various sources.
-    I was the second contributor to the codebase. We built data visualization tools and tackled complex data ingestion
-    challenges for the Ethiopian Ministry of Health.`,
+    name: 'IKEA',
+    position: 'Consultant, PM',
+    url: 'https://medium.com/studentsxstudents/what-i-learned-in-a-month-working-alongside-ikea-and-how-you-can-use-it-8e7cc7836b9b',
+    startDate: '2022-02-01',
+    endDate: '2022-03-01',
     highlights: [
-      'Worked in Addis Ababa for the Ethiopian Ministry of Health and built data visualization tools in React and Flask.',
-      'Ingested multiple databases with different alphabets, calendars, and without official spellings of geographic locations.',
+      'Tasked with identifying emergent trends in transportation and identifying the ideal store of 2030',
+      'Month-long intensive research and testing project included meeting with clients and creating economically sustainable business models',
+      'Created a full pitch deck outlining team\'s solution, presented to top IKEA execs',
+      'As Project Manager, assgined and delegated work, kept team on task and created deadlines for projects',
     ],
   },
   {
-    name: 'Matroid',
-    position: 'Co-founder',
-    url: 'https://matroid.com',
-    startDate: '2015-07-01',
-    endDate: '2016-01-01',
-    summary: `Matroid is a computer vision platform for creating and deploying detectors. Detectors help customers identify objects,
-    events, or patterns in images and videos without requiring machine learning or coding expertise. I played a crucial role in
-    defining the company's vision. I was responsible for architecting and building the initial product. I left shortly after the
-    Series A to focus on Arthena.`,
+    name: 'Jobby',
+    position: 'Intern',
+    url: 'https://www.linkedin.com/company/joinjobby/',
+    startDate: '2022-06-01',
+    endDate: '2022-07-01',
     highlights: [
-      'Developed end to end machine learning pipeline to train visual classifiers from keywords using Caffe, Node.JS, Redis, MongoDB, and other technologies.',
+      'Joined as an intern in early-stage startup, working with in an incubator environment',
+      'Worked on every aspect of the company, from outreach to user engagement and retention',
+      'Secured office tours at companies including KPMG, Scotiabank, IBM, CIBC and others',
     ],
   },
   {
-    name: 'Planet',
-    position: 'Missions Intern',
-    url: 'https://planet.com',
-    startDate: '2014-06-01',
-    endDate: '2015-01-01',
+    name: 'Geek Culture, StudentsxStudents, VisonaryHub',
+    position: 'Writer',
+    url: 'https://medium.com/@alexanderyevchenko',
+    startDate: '2021-09-01',
     highlights: [
-      'Built models to improve image quality, signal to noise ratio, and dynamic range.',
-      'Performed statistical analysis of image quality in Matlab and Python.  Developed flight software in C++.',
-      'Organized first hackathon, prototyped hardware, and designed and built photography equipment for rocket launches.',
-    ],
-  },
-  {
-    name: 'Planetary Resources',
-    position: 'Avionics Intern',
-    startDate: '2014-01-01',
-    endDate: '2014-05-01',
-    url: 'http://planetaryresources.com',
-    highlights: [
-      'Developed simulations in Matlab for Attitude Determination and Control Subsystem.',
-      'Developed processes for in lab testing and characterization of various subsystems.',
-      'Assembled flight hardware in cleanroom.',
-    ],
-  },
-  {
-    name: 'Facebook',
-    position: 'Software Engineer Intern',
-    url: 'https://facebook.com',
-    startDate: '2013-06-01',
-    endDate: '2013-09-01',
-    highlights: [
-      'Developed software in python for automated testing of servers.',
-      'Performed statistical analysis with R, HIVE to assist in triage of malfunctioning servers.',
-      'Worked with vendors and ODM\'s during triage to assist in risk mitigation.',
-    ],
-  },
-  {
-    name: 'SEDS-USA',
-    position: 'At Large Board Member',
-    url: 'http://seds.org',
-    startDate: '2013-10-01',
-    endDate: '2014-10-01',
-    highlights: [
-      'Elected to Board of Directors of the USA\'s largest student space advocacy group based on 5+ years of work with SEDS.',
-      'Responsibilities included: organizational strategy, conference presentations, fundraising, special projects, promoting SEDS nationally and internationally, and photographing conferences.',
-    ],
-  },
-  {
-    name: 'UB Nanosatellite Program',
-    position: 'Co-founder, Program Manager',
-    url: 'https://ubnl.space/',
-    startDate: '2010-10-01',
-    endDate: '2012-06-01',
-    highlights: [
-      'Coauthored grant to build a multi-spectral imaging satellite as part of the AFRL University Nanosatellite Program.',
-      'Lead a team of 60 students through satellite development life cycle. Served as a technical expert, acquired intimate working knowledge of satellite subsystems.  Solicited funding through NASA, AFOSR, and several corporate sponsors.',
+      'Write articles about emerging technology, philosophy, and personal growth, sharing knowledge and experiences with others',
+      'Reached an audience of 30,000+ people across all publications',
+      'Work featured in the National Observer and Forbes',
     ],
   },
 ];
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Main from '../layouts/Main';
-import EmailLink from '../components/Contact/EmailLink';
 import ContactIcons from '../components/Contact/ContactIcons';
 
 const Contact = () => (
   <Main
     title="Contact"
-    description="Contact Michael D'Angelo via email @ hi@mldangelo.com"
+    description="Contact ALexander Yevchenko via email at alexanderyevchenko.com"
   >
     <article className="post" id="contact">
       <header>
@@ -17,8 +16,15 @@ const Contact = () => (
         </div>
       </header>
       <div className="email-at">
-        <p>Feel free to get in touch. You can email me at: </p>
-        <EmailLink />
+        <p>Feel free to get in touch. You can message me at: </p>
+        <p><a href="mailto:alexanderyevchenko@gmail.com">alexanderyevchenko@gmail.com</a></p>
+        <p><a href="https://twitter.com/ayevch">@ayevc</a> on Twitter</p>
+        <p><a href="https://www.linkedin.com/in/alexanderyevchenko/">LinkedIn</a></p>
+        <br />
+        <p>Or check out my work here:</p>
+        <p><a href="https://substack.com/@yevchenko">Substack</a></p>
+        <p><a href="https://medium.com/@alexanderyevchenko">Medium</a></p>
+        <p><a href="https://www.youtube.com/channel/UCRD_fhqKNaTI8TGliToviGA">YouTube</a></p>
       </div>
       <ContactIcons />
     </article>
