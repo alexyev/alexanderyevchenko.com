d6022b5a2eea5eef594085df7ff5e6fdbbf5a66a
alexyev <85699087+alexyev@users.noreply.github.com>
2024-08-02 17:39:50 -0400

    update

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -3,7 +3,7 @@ const degrees = [
     school: 'The University of North Carolina, Chapel Hill',
     degree: 'B.A., Computer Science, Morehead-Cain Scholar',
     link: 'https://en.wikipedia.org/wiki/Morehead-Cain_Scholarship',
-    year: 'August 2024 - May 2028'
+    year: 'August 2024 - May 2028',
   },
   {
     school: 'Michael Power St. Joseph CHS',
